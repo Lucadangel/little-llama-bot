1f17ed6aba4023ebacd3b01dfbe34785f2bd4a8a
Luca D'Angelo <125386452+Lucadangel@users.noreply.github.com>
2026-02-27 23:01:10 +0100

    Add React ProductCarousel component

---
+++ src/components/ProductCarousel.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProductCarousel = () => {
+    // Add your carousel implementation here.
+    return (
+        <div className="product-carousel">
+            {/* Carousel items go here */}
+        </div>
+    );
+};
+
+export default ProductCarousel;
